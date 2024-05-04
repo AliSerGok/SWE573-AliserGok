1c3869e37afa6db840c6e8a9d4458ce087a37965
AliSerGok <98125147+AliSerGok@users.noreply.github.com>
2024-05-05 01:26:08 +0300

    Debuging
    debuging intellij idea error: could not find or load main class project.spiny caused by: java.lang.classnotfoundexception:

--- Spiny/Spiny/src/test/java/project/Spiny/SpinyApplicationTests.java
+++ Spiny/Spiny/src/test/java/project/Spiny/SpinyApplicationTests.java
@@ -1,9 +1,9 @@
 package project.Spiny;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.Test;
+
+
 class SpinyApplicationTests {
 
 	@Test
